7f49e510b1475aef743a51b9b8abb0b4e0564f9c
victoriaaoka <victoria.aoka@andela.com>
2017-06-14 10:39:21 +0300

    Reorganize files and add config file

---
+++ manage.py
@@ -0,0 +1,14 @@
+import os
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from my_app import db, create_app
+from my_app import models
+
+app = create_app(config_name=os.getenv('APP_SETTINGS'))
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
