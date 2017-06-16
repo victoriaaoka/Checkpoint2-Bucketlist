0a2d576aeb1f352529f7890d760bf9eec3f06296
victoriaaoka <victoria.aoka@andela.com>
2017-06-16 11:58:20 +0300

    Add SQLALCHEMY_DATABASE_URI

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,9 +11,10 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(app_config)
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/Bucketlistdb'
     db.init_app(app)
 
     return app
