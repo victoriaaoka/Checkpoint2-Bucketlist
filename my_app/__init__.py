e04f093f2eb4259a45e5d143895133a3e45769e3
victoriaaoka <victoria.aoka@andela.com>
2017-06-19 15:46:24 +0300

    Add bucktlists function to create and view bucketlists.

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,20 +1,51 @@
 import os
+from flask import request, jsonify, abort
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
-
-# local import
 from instance.config import app_config
-
-# initialize sql-alchemy
+from my_app.models import User, Bucketlist, BucketlistItem
 db = SQLAlchemy()
 
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object(app_config)
+    app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/Bucketlistdb'
     db.init_app(app)
 
+
+
+    @app.route('/bucketlists/', methods=['POST', 'GET'])
+    def bucketlists():
+        if request.method == "POST":
+            name = str(request.data.get('name', ''))
+            if name:
+                bucketlist = Bucketlist(name=name)
+                bucketlist.save()
+                response = jsonify({
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'created_by': bucketlist.created_by,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                })
+                response.status_code = 201
+                return response
+        else:
+            bucketlists = Bucketlist.get_all()
+            results = []
+
+            for bucketlist in bucketlists:
+                obj = {
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                }
+                results.append(obj)
+            response = jsonify(results)
+            response.status_code = 200
+            return response
     return app
