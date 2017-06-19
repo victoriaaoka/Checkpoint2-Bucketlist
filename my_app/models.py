789af5c26a9ad6a8faf15d76ac1fedf827fc9823
victoriaaoka <victoria.aoka@andela.com>
2017-06-19 15:47:00 +0300

    Edit to pep-8 styling.

--- my_app/models.py
+++ my_app/models.py
@@ -1,19 +1,21 @@
-from my_app import db
+import os
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 
 
 class User(db.Model):
     """This class represents the users table."""
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255))
     backetlists = db.relationship(
-        "Bucketlist", order_by="Bucketlist.id", cascade="all, delete-orphan")
+        'Bucketlist', order_by='Bucketlist.id', cascade='all, delete-orphan')
 
     def __init__(self, username):
-        """initialize with username."""
+        'initialize with username.'
         self.username = username
 
     def save(self):
@@ -32,7 +34,7 @@ class User(db.Model):
 class Bucketlist(db.Model):
     """This class represents the bucketlists table."""
 
-    __tablename__ = "bucketlists"
+    __tablename__ = 'bucketlists'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255))
@@ -42,11 +44,11 @@ class Bucketlist(db.Model):
         db.DateTime, default=db.func.current_timestamp(),
         onupdate=db.func.current_timestamp())
     items = db.relationship(
-        "BucketlistItem", order_by="BucketlistItem.bucketlist_id",
-        cascade="all, delete-orphan")
+        'BucketlistItem', order_by='BucketlistItem.bucketlist_id',
+        cascade='all, delete-orphan')
 
     def __init__(self, name):
-        """initialize with name."""
+        'initialize with name.'
         self.name = name
 
     def save(self):
@@ -62,13 +64,13 @@ class Bucketlist(db.Model):
         db.session.commit()
 
     def __repr__(self):
-        return "<Bucketlist: {}>".format(self.name)
+        return '<Bucketlist: {}>'.format(self.name)
 
 
 class BucketlistItem(db.Model):
     """This class represents the bucketlist items table."""
 
-    __tablename__ = "items"
+    __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255))
@@ -81,24 +83,24 @@ class BucketlistItem(db.Model):
         Bucketlist.id), nullable=False)
 
     def __init__(self, name):
-        """Initialize with name."""
+        'Initialize with name.'
         self.name = name
         self.bucketlist_id = bucketlist_id
 
     def save(self):
-        """Save a new/edited bucketlist item"""
+        'Save a new/edited bucketlist item'
         db.session.add(self)
         db.session.commit()
 
     @staticmethod
     def get_all(bucketlist_id):
-        """Retrieve all the bucketlists items in a given bucketlist"""
+        'Retrieve all the bucketlists items in a given bucketlist'
         return BucketListItem.query.filter_by(bucketlist_id=bucketlist_id)
 
     def delete(self):
-        """Delete a bucketlist item"""
+        'Delete a bucketlist item'
         db.session.delete(self)
         db.session.commit()
 
     def __repr__(self):
-        return "<BucketlistItem: {}>".format(self.name)
+        return '<BucketlistItem: {}>'.format(self.name)
