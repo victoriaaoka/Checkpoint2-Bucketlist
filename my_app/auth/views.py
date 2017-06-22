577e7d548354992bf166eaa10a45278cf47ff271
victoriaaoka <victoria.aoka@andela.com>
2017-06-22 15:11:03 +0300

    Add user login functionality.

--- my_app/auth/views.py
+++ my_app/auth/views.py
@@ -38,8 +38,48 @@ class RegistrationView(MethodView):
 
             return make_response(jsonify(response)), 202
 
+
+class LoginView(MethodView):
+    """This view handles user login and access token generation."""
+
+    def post(self):
+        """Handle POST request for login view."""
+        try:
+            user = User.query.filter_by(username=request.data['username']).first()
+
+            # Authenticate the user using their password
+            if user and user.password_is_valid(request.data['password']):
+                # Generate the access token.
+                access_token = user.generate_token(user.id)
+                if access_token:
+                    response = {
+                        'message': 'You logged in successfully.',
+                        'access_token': access_token
+                            }
+                    return make_response(jsonify(response)), 200
+            else:
+                response = {
+                    'message': 'Invalid username or password, Please try again'
+                }
+                return make_response(jsonify(response)), 401
+
+        except Exception as e:
+            response = {
+                'message': str(e)
+            }
+            return make_response(jsonify(response)), 500
+
+# Define the API resources
 registration_view = RegistrationView.as_view('register_view')
+login_view = LoginView.as_view('login_view')
+
+# Define the rule for the urls and then add the rule to the blueprint.
 auth_blueprint.add_url_rule(
     '/auth/register',
     view_func=registration_view,
+    methods=['POST'])
+
+auth_blueprint.add_url_rule(
+    '/auth/login',
+    view_func=login_view,
     methods=['POST'])
