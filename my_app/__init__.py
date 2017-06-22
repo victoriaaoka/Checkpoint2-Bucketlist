5314f23d0672589ed662610353e9c1764249c08f
victoriaaoka <victoria.aoka@andela.com>
2017-06-22 12:14:39 +0300

    Add bucketlist editing and deleting features. Add authentication blueprint.

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,21 +1,21 @@
 import os
 from flask import request, jsonify, abort
 from flask_api import FlaskAPI
+from flask import make_response
 from flask_sqlalchemy import SQLAlchemy
 from instance.config import app_config
 from my_app.models import User, Bucketlist, BucketlistItem
-db = SQLAlchemy()
+from my_app.models import db
+from .auth import auth_blueprint
 
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(os.getenv('DATABASE_URL'))
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/Bucketlistdb'
     db.init_app(app)
-
-
 
     @app.route('/bucketlists/', methods=['POST', 'GET'])
     def bucketlists():
@@ -48,4 +48,40 @@ def create_app(config_name):
             response = jsonify(results)
             response.status_code = 200
             return response
+
+    @app.route('/bucketlists/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+    def bucketlist_manipulation(id, **kwargs):
+        bucketlist = Bucketlist.query.filter_by(id=id).first()
+        if not bucketlist:
+            abort(404)
+            return make_response(jsonify(response)), 404
+
+        if request.method == 'DELETE':
+            bucketlist.delete()
+            return {
+            "message": "bucketlist {} deleted successfully".format(bucketlist.id)
+         }, 200
+
+        elif request.method == 'PUT':
+            name = str(request.data.get('name', ''))
+            bucketlist.name = name
+            bucketlist.save()
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.name,
+                'date_created': bucketlist.date_created,
+                'date_modified': bucketlist.date_modified
+            })
+            response.status_code = 200
+            return response
+        else:
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.name,
+                'date_created': bucketlist.date_created,
+                'date_modified': bucketlist.date_modified
+            })
+            response.status_code = 200
+            return response
+    app.register_blueprint(auth_blueprint)
     return app
