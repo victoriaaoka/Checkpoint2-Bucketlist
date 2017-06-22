de9441324fac93c62b77eaf0341a82c125dead81
victoriaaoka <victoria.aoka@andela.com>
2017-06-22 12:08:53 +0300

    Add runtests command

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 import os
 from flask_script import Manager
+import unittest
 from flask_migrate import Migrate, MigrateCommand
 from my_app import db, create_app
 from my_app import models
@@ -10,6 +11,15 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+
+@manager.command
+def runtests():
+    """Runs the unit tests automatically."""
+    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     manager.run()
