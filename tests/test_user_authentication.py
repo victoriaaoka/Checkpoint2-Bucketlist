93c1ad7bff616e91d1cc87ef1ea6df15049a37a6
victoriaaoka <victoria.aoka@andela.com>
2017-06-30 13:02:06 +0300

    [Chore #147018467]Add tests for edge cases regarding the tests defence feedback.

--- tests/test_user_authentication.py
+++ tests/test_user_authentication.py
@@ -15,49 +15,111 @@ class AuthTestCase(unittest.TestCase):
             'email': 'v@gmail.com',
             'password': 'password'
         }
+        self.user_login = {
+            'username': 'vicky',
+            'password': 'password'
 
+        }
+        with self.app.app_context():
+            db.create_all()
+
+    def tearDown(self):
+        """reset all initialized variables."""
         with self.app.app_context():
             db.session.close()
             db.drop_all()
-            db.create_all()
 
     def test_successful_registration(self):
         """Test successful user registration."""
-        response = self.client().post('/auth/register', data=self.user_data)
-        result = json.loads(response.data.decode())
-        self.assertEqual(result['message'], "You registered successfully!")
+        response = self.client().post('v1/auth/register', data=json.dumps(
+            self.user_data))
+        self.assertEqual(response, "You registered successfully!")
         self.assertEqual(response.status_code, 201)
+
+    def test_register_without_username_or_password(self):
+        """Test the registration of a user without a username and password."""
+        new_user = {
+            'email': 'vicky@gmail.com',
+            'username': ' ',
+            'password': ' '
+        }
+        response = self.client().post('/auth/register', data=json.dumps(new_user))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response, "Please fill in the required fields.")
+
+    def test_register_with_username_not_string(self):
+        """Test the registration of a user with a username that is not string."""
+        new_user = {
+            'email': 'vicky@gmail.com',
+            'username': '@#$$54678',
+            'password': 'password'
+        }
+        response = self.client().post('/auth/register', data=json.dumps(new_user))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response, "The user name can only be a string.")
+
+    def test_register_with_invalid_email(self):
+        """Test the registration of a user with an invalid email."""
+        new_user = {
+            'email': 'vicky5678',
+            'username': ' vicky',
+            'password': ' password'
+        }
+        response = self.client().post('/auth/register', data=json.dumps(new_user))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response, "Invalid email address.")
+
+    def test_register_with_short_password(self):
+        """Test the registration of a user with a very short password."""
+        new_user = {
+            'email': 'vicky@gmail.com',
+            'username': ' vicky',
+            'password': ' pass'
+        }
+        response = self.client().post('/auth/register', data=json.dumps(new_user))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response, "The password is too short.")
 
     def test_registering_a_user_who_already_exists(self):
         """Test that a user cannot be registered twice."""
-        response = self.client().post('/auth/register', data=self.user_data)
-        self.assertEqual(response.status_code, 201)
-        second_response = self.client().post('/auth/register', data=self.user_data)
-        self.assertEqual(second_response.status_code, 202)
-        result = json.loads(second_response.data.decode())
+        self.client().post('v1/auth/register', data=json.dumps(self.user_data))
+        response = self.client().post('v1/auth/register', data=json.dumps(
+            self.user_data))
+        self.assertEqual(response.status_code, 409)
         self.assertEqual(
-            result['message'], "The user already exists! Please login.")
+            response['message'], "The user already exists! Please login.")
 
     def test_registered_user_can_login(self):
         """Test registered user can login successfully."""
-        response = self.client().post('/auth/register', data=self.user_data)
-        self.assertEqual(response.status_code, 201)
-        login_response = self.client().post('/auth/login', data=self.user_data)
-        result = json.loads(login_response.data.decode())
-        self.assertEqual(login_response.status_code, 200)
-        self.assertEqual(result['message'], "You logged in successfully.")
-        self.assertTrue(result['access_token'])
+        self.client().post('v1/auth/register', data=json.dumps(self.user_data))
+        response = self.client().post('v1/auth/login', data=json.dumps(
+            self.user_login))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['message'], "You logged in successfully.")
+        self.assertTrue(response['access_token'])
 
     def test_non_registered_user_login(self):
         """Test non registered users cannot login."""
         not_registered = {
             'username': 'notregistered',
-            'email': 'not_registered@example.com',
             'password': 'password'
         }
 
-        response = self.client().post('/auth/login', data=not_registered)
+        response = self.client().post('v1/auth/login', data=json.dumps(
+            not_registered))
         self.assertEqual(response.status_code, 401)
-        result = json.loads(response.data.decode())
         self.assertEqual(
-            result['message'], "Invalid username or password, Please try again")
+            response['message'], "The user does not exist.")
+
+    def test_login_without_username_or_password(self):
+        """Test user login without username and password."""
+        self.client().post('/auth/register', data=json.dumps(self.user_data))
+        response = self.client().post('v1/auth/login', data={
+            'username': ' ', 'password': ' '})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response['message'], "Please enter Username and Password.")
+        self.assertFalse(response['access_token'])
