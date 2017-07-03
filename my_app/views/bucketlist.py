09bc39ed147503ef1d031bc11954c320c7b144e0
victoriaaoka <victoria.aoka@andela.com>
2017-07-04 00:09:24 +0300

    [Feature #147018689]Refactor code to use blueprints and add post,get,put and delete methods.

--- my_app/views/bucketlist.py
+++ my_app/views/bucketlist.py
@@ -1,9 +1,15 @@
 from flask.views import MethodView
 from flask import request, jsonify, abort, make_response
+<<<<<<< HEAD
+=======
+
+#from decorator import login_required
+>>>>>>> d3e88accb16acddc0c18794e342bc23c89284613
 from my_app.models import Bucketlist, BucketlistItem
 from . import bucketlist_blueprint
 
 
+<<<<<<< HEAD
 class BucketlistView(MethodView):
 
     def post(self):
@@ -53,6 +59,56 @@ class BucketlistManipulationView(MethodView):
             return make_response(jsonify(response)), 404
         else:
             name = str(request.data.get('name'))
+=======
+class BucketListView(MethodView):
+    #decorators = [login_required]
+
+    def bucketlists():
+        if request.method == "POST":
+            name = str(request.data.get('name', ''))
+            if name:
+                bucketlist = Bucketlist(name=name)
+                bucketlist.save()
+                response = jsonify({
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'created_by': bucketlist.created_by,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                })
+                response.status_code = 201
+                return response
+        else:
+            bucketlists = Bucketlist.get_all()
+            results = []
+
+            for bucketlist in bucketlists:
+                obj = {
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                }
+                results.append(obj)
+            response = jsonify(results)
+            response.status_code = 200
+            return response
+
+    def bucketlist_manipulation(id, **kwargs):
+        bucketlist = Bucketlist.query.filter_by(id=id).first()
+        if not bucketlist:
+            abort(404)
+            return make_response(jsonify(response)), 404
+
+        if request.method == 'DELETE':
+            bucketlist.delete()
+            return {
+            "message": "bucketlist {} deleted successfully".format(bucketlist.id)
+         }, 200
+
+        elif request.method == 'PUT':
+            name = str(request.data.get('name', ''))
+>>>>>>> d3e88accb16acddc0c18794e342bc23c89284613
             bucketlist.name = name
             bucketlist.save()
             response = jsonify({
@@ -61,6 +117,7 @@ class BucketlistManipulationView(MethodView):
                 'date_created': bucketlist.date_created,
                 'date_modified': bucketlist.date_modified
             })
+<<<<<<< HEAD
             response = {"message": "Bucketlist updated successfully."}
             return make_response(jsonify(response)), 200
 
@@ -85,3 +142,27 @@ bucketlist_blueprint.add_url_rule("/bucketlists/", view_func=bucketlist_view,
 bucketlist_blueprint.add_url_rule(
     "/bucketlists/<int:id>", view_func=manipulation_view,
     methods=['DELETE', 'PUT'])
+=======
+            response.status_code = 200
+            return response
+        else:
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.name,
+                'date_created': bucketlist.date_created,
+                'date_modified': bucketlist.date_modified
+            })
+            response.status_code = 200
+            return response
+
+#  # API resource
+# bucketlist_view = BucketListView.as_view("bucketlist_view")
+# manipulation_view = BucketListManipulationView.as_view("manipulation_view")
+
+# # Rule for bucketlist with blueprint
+# bucketlist_blueprint.add_url_rule("/bucketlists/", view_func=bucketlist_view,
+#                                   methods=["POST", "GET"])
+# bucketlist_blueprint.add_url_rule(
+#     "/bucketlists/<int:id>/", view_func=manipulation_view,
+#     methods=["PUT", "GET", "DELETE"])
+>>>>>>> d3e88accb16acddc0c18794e342bc23c89284613
