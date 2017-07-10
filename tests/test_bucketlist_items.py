fc5831793214b22e1db5a742964fe5512647d9b2
victoriaaoka <victoria.aoka@andela.com>
2017-07-10 14:46:51 +0300

    [chore] refactor tests.

--- tests/test_bucketlist_items.py
+++ tests/test_bucketlist_items.py
@@ -3,7 +3,7 @@ import os
 import sys
 import json
 sys.path.append('../')
-from my_app import create_app, db
+from my_app.app import create_app, db
 
 
 class BucketlistItemTestCase(unittest.TestCase):
@@ -13,6 +13,11 @@ class BucketlistItemTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app(config_name='testing')
         self.client = self.app.test_client
+
+        self.cntx = self.app.app_context()
+        self.cntx.push()
+        db.drop_all()
+        db.create_all()
         self.bucketlist = {'name': 'Vacation'}
         self.bucketlist_item = {'name': 'Go to Tokyo', 'done': False}
         self.user_data = {
@@ -25,104 +30,103 @@ class BucketlistItemTestCase(unittest.TestCase):
             'password': 'password'
 
         }
-        self.client().post('v1/auth/register', data=json.dumps(
-            self.user_data))
-        self.client().post('v1/auth/login', data=json.dumps(
-            self.user_login))
-
-        with self.app.app_context():
-            db.create_all()
+        self.client().post('/api/v1/auth/register', data=
+            self.user_data)
 
     def tearDown(self):
         """reset all initialized variables."""
-        with self.app.app_context():
-            db.session.remove()
-            db.drop_all()
+        db.session.remove()
+        db.drop_all()
+        self.cntx.pop()
+
+
+    def get_token(self):
+        """Return authentication token."""
+        response = self.client().post("/api/v1/auth/login",
+                          data=self.user_login)
+        output = json.loads(response.data.decode())
+        token = output["access_token"]
+        return {"access_token": token}
 
     def test_create_a_bucketlist_item_successfully(self):
         """ Tests endpoint can create new bucketlist item."""
-        self.client().post('/bucketlists/', data=json.dumps(self.bucketlist))
-        response = self.client().post('/bucketlists/1/items/', data=json.dumps(
-             self.bucketlist_item))
+        self.client().post('/api/v1/bucketlists/',
+            data = self.bucketlist, headers=self.get_token())
+        response = self.client().post('/api/v1/bucketlists/1/items/', data=
+             self.bucketlist_item, headers=self.get_token())
         self.assertEqual(response.status_code, 201)
-        self.assertIn('Tokyo', str(response.data))
+        output = json.loads(response.data.decode())
+        self.assertIn('Tokyo', output['name'])
 
     def test_create_a_bucketlist_item_without_name(self):
         """ Tests endpoint can create new bucketlist item."""
         new_bucketlist_item = {'name': ' '}
-        self.client().post('/bucketlists/', data=json.dumps(self.bucketlist))
-        response = self.client().post('/bucketlists/1/items/', data=json.dumps(
-             new_bucketlist_item))
+        self.client().post('/api/v1/bucketlists/',
+            data=self.bucketlist, headers=self.get_token())
+        response = self.client().post('/api/v1/bucketlists/1/items/', data=
+             new_bucketlist_item, headers=self.get_token())
         self.assertEqual(response.status_code, 400)
+        output = json.loads(response.data.decode())
         self.assertEqual(
-            response['message'], "The bucketlist item name should be entered! ")
+            output['name'][0], "Shorter than minimum length 3.")
 
     def test_create_a_bucketlist_item_that_exists(self):
         """ Tests the creation of a bucketlist item that already exists."""
-        self.client().post('/bucketlists/', data=json.dumps(self.bucketlist))
-        self.client().post('/bucketlists/1/items/', data=json.dumps(
-             self.bucketlist_item))
-        response = self.client().post('/bucketlists/1/items/', data=json.dumps(
-             self.bucketlist_item))
+        self.client().post('/api/v1/bucketlists/',
+            data=self.bucketlist, headers=self.get_token())
+        self.client().post('/api/v1/bucketlists/1/items/', data=
+             self.bucketlist_item, headers=self.get_token())
+        response = self.client().post('/api/v1/bucketlists/1/items/', data=
+             self.bucketlist_item, headers=self.get_token())
         self.assertEqual(response.status_code, 409)
+        output = json.loads(response.data.decode())
         self.assertEqual(
-            response['message'], "The bucketlist item already exists! ")
-
-    def test_api_can_get_all_bucketlist_items(self):
-        """Test that the API can get all the bucketlist items."""
-        self.client().post('/bucketlists/', data=json.dumps(self.bucketlist))
-        self.client().post('/bucketlists/1/items/', data=json.dumps(
-            self.bucketlist_item))
-        response = self.client().get('/bucketlists/1/items/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('Tokyo', str(response.data))
-
-    def test_retrieving_bucketlist_items_when_none_exists(self):
-        """Test retrieving bucketlist items when none exists."""
-        self.client().post('/bucketlists/', data=json.dumps(self.bucketlist))
-        response = self.client().get('/bucketlists/1/items/')
-        self.assertEqual(response.status_code, 404)
-        self.assertEqual(
-            response['message'], "There are no bucketlist items created yet.")
+            output['message'], "The bucketlist item already exists!")
 
     def test_update_a_bucketlist_item(self):
         """Tests that a bucketlist item can be updated."""
-        self.client().post('/bucketlists/', data=json.dumps(self.bucketlist))
-        self.client().post('/bucketlists/1/items/', data=json.dumps(
-            self.bucketlist_item))
-        response = self.client().put('/bucketlists/1/items/1', data={
-            'name': 'Go to Mombasa!'})
+        self.client().post('/api/v1/bucketlists/',
+            data=self.bucketlist, headers=self.get_token())
+        self.client().post('/api/v1/bucketlists/1/items/',
+            data=self.bucketlist_item, headers=self.get_token())
+        response = self.client().put('/api/v1/bucketlists/1/items/1', data={
+            'name': 'Go to Mombasa!'}, headers=self.get_token())
         self.assertEqual(response.status_code, 200)
-        result = self.client().get('/bucketlists/1/items/1')
-        self.assertIn('Mombasa', str(result.data))
 
     def test_edit_a_bucketlist_item_that_does_not_exist(self):
         """Tests that a bucketlist item that does not exist can not be updated."""
-        self.client().post('/bucketlists/', data=json.dumps(self.bucketlist))
-        response = self.client().put('/bucketlists/1/items/1', data={
-            'name': 'Go to Mombasa!'})
-        self.assertEqual(response.status_code, 400)
+        self.client().post('/api/v1/bucketlists/',
+            data=self.bucketlist, headers=self.get_token())
+        response = self.client().put('/api/v1/bucketlists/1/items/1', data={
+            'name': 'Go to Mombasa!'}, headers=self.get_token())
+        self.assertEqual(response.status_code, 404)
 
     def test_update_a_bucketlist_item_with_same_data(self):
         """Tests that a bucketlist item cannot be updated with the same data."""
-        self.client().post('/bucketlists/', data=json.dumps(self.bucketlist))
-        self.client().post('/bucketlists/1/items/', data=json.dumps(
-            self.bucketlist_item))
-        response = self.client().put('/bucketlists/1/items/1', data={
-            'name': 'Go to Tokyo'})
+        self.client().post('/api/v1/bucketlists/', data=self.bucketlist, headers=self.get_token())
+        self.client().post('/api/v1/bucketlists/1/items/', data=
+            self.bucketlist_item, headers=self.get_token())
+        response = self.client().put('/api/v1/bucketlists/1/items/1', data={
+            'name': 'Go to Tokyo'}, headers=self.get_token())
         self.assertEqual(response.status_code, 409)
 
     def test_successful_bucketlist_item_deletion(self):
         """Test that the API can delete an existing bucketlist."""
-        self.client().post('/bucketlists/', data={'name': 'Go shopping'})
-        self.client().post('/bucketlists/1/items/', data=json.dumps(
-            self.bucketlist_item))
-        response = self.client().delete('/bucketlists/1/items/1')
+        self.client().post('/api/v1/bucketlists/', data={'name': 'Go shopping'},
+            headers=self.get_token())
+        self.client().post('/api/v1/bucketlists/1/items/',
+            data=self.bucketlist_item, headers=self.get_token())
+        response = self.client().delete('/api/v1/bucketlists/1/items/1',
+            headers=self.get_token())
         self.assertEqual(response.status_code, 200)
-        result = self.client().get('/bucketlists/1/items/1')
+        result = self.client().delete('/api/v1/bucketlists/1/items/1',
+            headers=self.get_token())
         self.assertEqual(result.status_code, 404)
 
     def test_delete_bucketlist_item_that_does_not_exist(self):
         """Test the deletion of a bucketlist item that does not exist."""
-        resp = self.client().delete('/bucketlists/1/items/100')
+        self.client().post('/api/v1/bucketlists/', data={'name': 'Go shopping'},
+            headers=self.get_token())
+        resp = self.client().delete('/api/v1/bucketlists/1/items/2',
+            headers=self.get_token())
         self.assertEqual(resp.status_code, 404)
