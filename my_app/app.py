deb9f059c7a60a8a66a2c6f5273994b45abeb88f
victoriaaoka <victoria.aoka@andela.com>
2017-07-10 14:48:35 +0300

    [chore] Update Reorganize code. add create app and db.

---
+++ my_app/app.py
@@ -0,0 +1,24 @@
+import os
+from flask_api import FlaskAPI
+from flask_sqlalchemy import SQLAlchemy
+from instance.config import app_config
+
+
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = FlaskAPI(__name__, instance_relative_config=True)
+    app.config.from_object(os.getenv('DATABASE_URL'))
+    app.config.from_pyfile('config.py')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.get('SECRET')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/Bucketlistdb'
+    db.init_app(app)
+
+    from my_app.views import auth_blueprint, bucketlist_blueprint, bucketlist_item_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/api/v1')
+    app.register_blueprint(bucketlist_blueprint, url_prefix='/api/v1')
+    app.register_blueprint(bucketlist_item_blueprint, url_prefix='/api/v1')
+
+    return app
