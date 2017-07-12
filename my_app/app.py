ea3a8b6f566a784470e7b3c9b81d5486cc7be9eb
victoriaaoka <victoria.aoka@andela.com>
2017-07-12 18:28:39 +0300

    [chore] Add link to the API documentstion.

--- my_app/app.py
+++ my_app/app.py
@@ -1,4 +1,5 @@
 import os
+from flask import redirect, url_for
 from flask_api import FlaskAPI
 from flasgger import Swagger
 from flask_sqlalchemy import SQLAlchemy
@@ -18,7 +19,10 @@ def create_app(config_name):
     # app.config.get('SECRET')
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/Bucketlistdb'
     db.init_app(app)
-
+    @app.route("/")
+    def index():
+        url = url_for("flasgger.apidocs")
+        return redirect(url,  code=302)
     from my_app.views import auth_blueprint, bucketlist_blueprint, bucketlist_item_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/api/v1')
     app.register_blueprint(bucketlist_blueprint, url_prefix='/api/v1')
