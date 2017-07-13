589fd50ac51d3d6d5339729997ad4c11dfdc9caa
victoriaaoka <victoria.aoka@andela.com>
2017-07-13 12:00:40 +0300

    [chore ##148817477]Edit code to pep8.

--- my_app/schema.py
+++ my_app/schema.py
@@ -2,45 +2,53 @@ from marshmallow import Schema, fields, validate
 
 
 class UserRegistrationSchema(Schema):
+    """This class validates user input for user registration."""
 
-    username = fields.String(validate=[validate.Length(min=3),
-                              validate.Regexp(r"[a-zA-Z0-9_\- ]*$",
-                                              error="Invalid characters entered.")],
-                              required=True,
-                              error_messages={"required": "Please enter a username"})
+    username = fields.String(
+      validate=[validate.Length(min=3), validate.Regexp(
+        r'[a-zA-Z0-9_\- ]*$',
+        error='Invalid characters entered.')],
+      required=True,
+      error_messages={'required': 'Please enter a username'})
     email = fields.Email(validate=[validate.Length(max=30)],
                          required=True,
-                         error_messages={"required": "Please enter an email address"})
+                         error_messages={
+                         'required': 'Please enter an email address'})
     password = fields.String(validate=[validate.Length(min=6)],
                              required=True,
-                             error_messages={"required": "Please enter a password"})
+                             error_messages={
+                             'required': 'Please enter a password'})
 
 
 class UserLoginSchema(Schema):
+    """This class validates user input for user login."""
 
-    username = fields.String(validate=[validate.Length(min=3),
-                        validate.Regexp(r"[a-zA-Z0-9_\- ]*$",
-                                              error="Invalid characters entered.")],
-                              required=True,
-                              error_messages={"required": "Please enter a username"})
+    username = fields.String(
+      validate=[validate.Length(min=3), validate.Regexp(
+        r'[a-zA-Z0-9_\- ]*$',
+        error='Invalid characters entered.')],
+      required=True,
+      error_messages={'required': 'Please enter a username'})
     password = fields.String(validate=[validate.Length(min=5)],
                              required=True,
-                             error_messages={"required": "Enter password"})
+                             error_messages={'required': 'Enter password'})
 
 
 class BucketlistSchema(Schema):
+    """This class validates user input for bucketlist creation."""
 
-    name = fields.String(validate=[validate.Length(min=3),
-                                        validate.Regexp(r"[a-zA-Z0-9_\- ]*$",
-                                         error="Invalid characters")],
-                         required=True,
-                         error_messages={"required": "Please enter the bucketlist name"})
+    name = fields.String(validate=[validate.Length(min=3), validate.Regexp(
+      r'[a-zA-Z0-9_\- ]*$',
+      error='Invalid characters')],
+      required=True,
+      error_messages={'required': 'Please enter the bucketlist name'})
 
 
 class BucketlistItemSchema(Schema):
+    """This class validates user input for bucketlist item creation."""
 
-    name = fields.String(validate=[validate.Length(min=3),
-                                        validate.Regexp(r"[a-zA-Z0-9_\- ]*$",
-                                         error="Invalid characters")],
-                         required=True,
-                         error_messages={"required": "Please enter an item name"})
+    name = fields.String(validate=[validate.Length(min=3), validate.Regexp(
+      r'[a-zA-Z0-9_\- ]*$',
+      error='Invalid characters')],
+      required=True,
+      error_messages={'required': 'Please enter an item name'})
