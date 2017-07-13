3d8ca0eb116713fbfa3d554b59681f699e92273f
victoriaaoka <victoria.aoka@andela.com>
2017-07-13 12:00:27 +0300

    [chore ##148817477]Edit code to pep8.

--- decorator.py
+++ decorator.py
@@ -8,18 +8,18 @@ def login_required(func):
 
     @wraps(func)
     def decorator(*args, **kwargs):
-        access_token = request.headers.get("access_token")
+        access_token = request.headers.get('access_token')
         if not access_token:
-            response = {"message": "No token provided."}
+            response = {'message': 'No token provided.'}
             return make_response(jsonify(response)), 401
 
         if access_token:
             # decode token and get the User ID
-            kwargs["user_id"] = User.decode_token(access_token)
-            if not isinstance(kwargs["user_id"], int):
-                message = kwargs["user_id"]
+            kwargs['user_id'] = User.decode_token(access_token)
+            if not isinstance(kwargs['user_id'], int):
+                message = kwargs['user_id']
                 response = {
-                    "message": message
+                    'message': message
                 }
                 return make_response(jsonify(response)), 401
 
