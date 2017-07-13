27291c6b3706f389dffdb38e6e91b1bc1968a6d7
victoriaaoka <victoria.aoka@andela.com>
2017-07-13 07:14:35 +0300

    [chore] Edit to pep8.

--- my_app/views/__init__.py
+++ my_app/views/__init__.py
@@ -4,9 +4,9 @@ from flask import Blueprint
 auth_blueprint = Blueprint('auth', __name__)
 from . import auth
 
-bucketlist_blueprint = Blueprint("bucketlist", __name__)
+bucketlist_blueprint = Blueprint('bucketlist', __name__)
 from . import bucketlist
 
 
-bucketlist_item_blueprint = Blueprint("bucketlist_item", __name__)
+bucketlist_item_blueprint = Blueprint('bucketlist_item', __name__)
 from . import bucketlist_item
