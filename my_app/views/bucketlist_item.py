45d2c1cb53692e0ecb523c985b4422dfa88cd0bc
victoriaaoka <victoria.aoka@andela.com>
2017-07-13 07:16:50 +0300

    [chore #148731347]Add API documentation and edit to pep8.

--- my_app/views/bucketlist_item.py
+++ my_app/views/bucketlist_item.py
@@ -15,6 +15,20 @@ class BucketlistItemView(MethodView):
        ---
        tags:
             - The Bucketlist Items API
+       parameters:
+         - in: header
+           name: access_token
+           type: string
+           required: true
+         - in: formData
+           name: name
+           type: string
+           required: true
+         - in: path
+           name: id
+           type: integer
+           required: true
+           description: Bucketlist id
        responses:
          201:
            description: New bucketlist item created.
@@ -23,7 +37,7 @@ class BucketlistItemView(MethodView):
         bucketlists = Bucketlist.query.filter_by(created_by=user_id).all()
         bucketlist = bucketlists[id - 1]
         if not bucketlist:
-            response = {"message": "You do not have any bucketlists."}
+            response = {'message': 'You do not have any bucketlists.'}
             return make_response(jsonify(response)), 404
         else:
             name = str(request.data.get('name'))
@@ -36,12 +50,15 @@ class BucketlistItemView(MethodView):
                 existing_item = BucketlistItem.query.filter_by(
                     name=name,).first()
                 if existing_item:
-                    response = {"message": "The bucketlist item already exists!"}
+                    response = {'message': 'The bucketlist \
+item already exists!'}
                     return make_response(jsonify(response)), 409
                 else:
-                    item = BucketlistItem(name=name, bucketlist_id=bucketlist.id)
+                    item = BucketlistItem(
+                        name=name, bucketlist_id=bucketlist.id)
                     item.save()
-                    items = BucketlistItem.query.filter_by(bucketlist_id=bucketlist.id).all()
+                    items = BucketlistItem.query.filter_by(
+                        bucketlist_id=bucketlist.id).all()
                     response = {
                         'id': len(items),
                         'name': item.name,
@@ -50,7 +67,7 @@ class BucketlistItemView(MethodView):
                         'bucketlist_id': id
                     }
                     response.update(
-                        {"message": "The bucketlist item has been created!"})
+                        {'message': 'The bucketlist item has been created!'})
                     return make_response(jsonify(response)), 201
             else:
                 response = {"message": "Please enter bucketlist item name."}
@@ -67,21 +84,45 @@ class BucketlistItemManipulationView(MethodView):
        ---
        tags:
             - The Bucketlist Items API
+       parameters:
+         - in: header
+           name: access_token
+           type: string
+           required: true
+         - in: path
+           name: id
+           type: integer
+           required: true
+           description: Bucketlist id
+         - in: path
+           name: item_id
+           type: integer
+           required: true
+         - in: formData
+           name: name
+           required: required
+           type: string
+           description: The item cannot be updated with same data.
        responses:
          201:
            description: Bucketlist item updated.
 
         """
-        bucketlists = Bucketlist.query.filter_by(created_by=user_id).all()
-        bucketlist = bucketlists[id - 1]
-        items = BucketlistItem.query.filter_by(
-            bucketlist_id=bucketlist.id).all()
+        try:
+            bucketlists = Bucketlist.query.filter_by(created_by=user_id).all()
+            bucketlist = bucketlists[id - 1]
+            items = BucketlistItem.query.filter_by(
+                bucketlist_id=bucketlist.id).all()
+        except IndexError:
+            response = {'message': 'The bucketlist does not exist.'}
+            return make_response(jsonify(response)), 404
         try:
             item = items[item_id-1]
             if item:
                 name = str(request.data.get('name', ''))
                 if item.name == name:
-                    response = {"message": "The bucketlist cannot be updated with same data."}
+                    response = {'message': 'The bucketlist cannot \
+be updated with same data.'}
                     return make_response(jsonify(response)), 409
                 else:
                     item.name = name
@@ -92,10 +133,11 @@ class BucketlistItemManipulationView(MethodView):
                             'date_created': item.date_created,
                             'date_modified': item.date_modified
                     }
-                    response .update({"message": "Bucketlist item updated successfully."})
+                    response .update({'message': 'Bucketlist item \
+updated successfully.'})
                     return make_response(jsonify(response)), 200
         except IndexError:
-            response = {"message": "The bucketlist item does not exist."}
+            response = {'message': 'The bucketlist item does not exist.'}
             return make_response(jsonify(response)), 404
 
     def delete(self, id, item_id, user_id):
@@ -104,6 +146,19 @@ class BucketlistItemManipulationView(MethodView):
        ---
        tags:
             - The Bucketlist Items API
+       parameters:
+         - in: header
+           name: access_token
+           type: string
+           required: true
+         - in: path
+           name: id
+           type: integer
+           required: true
+         - in: path
+           name: item_id
+           type: integer
+           required: true
        responses:
          201:
            description: Bucketlist item deleted.
@@ -115,22 +170,21 @@ class BucketlistItemManipulationView(MethodView):
             bucketlist_id=bucketlist.id).all()
         try:
             item = items[item_id-1]
-            if item:
-                item.delete()
-                response = {"message": "Bucketlist item deleted successfully."}
-                return make_response(jsonify(response)), 200
+            item.delete()
+            response = {'message': 'Bucketlist item deleted successfully.'}
+            return make_response(jsonify(response)), 200
         except IndexError:
-            response = {"message": "The bucketlist item does not exist."}
+            response = {'message': 'The bucketlist item does not exist.'}
             return make_response(jsonify(response)), 404
 
 # API resource
-bucketlist_item_view = BucketlistItemView.as_view("bucketlist_item_view")
-manipulation_view = BucketlistItemManipulationView.as_view("manipulation_view")
+bucketlist_item_view = BucketlistItemView.as_view('bucketlist_item_view')
+manipulation_view = BucketlistItemManipulationView.as_view('manipulation_view')
 
-# Rule for bucketlist with blueprint
+# Define the rule for the urls and then add the rule to the blueprint.
 bucketlist_item_blueprint.add_url_rule(
-    "/bucketlists/<int:id>/items/", view_func=bucketlist_item_view,
-    methods=["POST"])
+    '/bucketlists/<int:id>/items/', view_func=bucketlist_item_view,
+    methods=['POST'])
 bucketlist_item_blueprint.add_url_rule(
-    "/bucketlists/<int:id>/items/<int:item_id>", view_func=manipulation_view,
-    methods=["PUT", "DELETE"])
+    '/bucketlists/<int:id>/items/<int:item_id>', view_func=manipulation_view,
+    methods=['PUT', 'DELETE'])
