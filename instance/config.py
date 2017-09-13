82c4c52f4ae0a2fa7ece5c3f75be82b15fee7a3e
victoriaaoka <victoria.aoka@andela.com>
2017-09-13 16:40:07 +0300

    Update config file

--- instance/config.py
+++ instance/config.py
@@ -22,7 +22,7 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or "postgres://localhost/test_db"
+    # SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or "postgres://localhost/test_db"
     DEBUG = True
 
 
