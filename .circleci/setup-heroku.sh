7840bad02f40cfc0efc17637403f4824b287d7eb
victoriaaoka <victoria.aoka@andela.com>
2017-11-28 03:11:30 +0300

    Edit project name

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git remote add heroku https://git.heroku.com/todos-prod.git
+git remote add heroku https://git.heroku.com/Checkpoint2_Bucketlist.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 sudo mkdir -p /usr/local/lib /usr/local/bin
 sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
